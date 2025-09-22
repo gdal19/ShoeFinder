90f7ddcfd02fc3cfc4879955f3c1bcd01e29932b
gdal19 <guidaltrino@gmail.com>
2025-09-22 20:49:26 -0300

    First Commit

---
+++ src/Handler.java
@@ -0,0 +1,19 @@
+package src;
+
+public class Handler {
+    public void GetShoe(){
+
+    }
+
+    public void AddShoe(){
+
+    }
+
+    public void RemoveShoe(){
+
+    }
+
+    public void EditShoe(){
+        
+    }
+}
