858b00755d2f2c415d63ca72d31a329c736c5da1
Gui <guidaltrino@gmail.com>
2025-09-28 23:41:35 -0300

    Starting to build HTTP Server

---
+++ src/main/java/com/shoefinder/HttpServer.java
@@ -0,0 +1,12 @@
+package src.main.java.com.shoefinder;
+
+//Driver class for the HTTP Server
+
+public class HttpServer {
+
+    public static void main (String[] args) {
+        System.out.println("Server starting on port");
+
+    }
+
+}
