d8f2802e3edf97f9726abecb234e37b1753086d2
Gui <guidaltrino@gmail.com>
2026-02-15 14:43:07 -0300

    Creating initial  Domains

---
+++ src/main/java/com/shoefinder/domain/Shoe.java
@@ -0,0 +1,50 @@
+package com.shoefinder.domain;
+
+import jakarta.persistence.*;
+import java.time.LocalDate;
+
+@Entity
+@Table("shoes")
+public class Shoe {
+
+    private String name;
+    private String brand;
+    private int price;
+    private String color;
+    private LocalDate releaseDate;
+
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getBrand() {
+        return brand;
+    }
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+    public int getPrice() {
+        return price;
+    }
+    public void setPrice(int price) {
+        this.price = price;
+    }
+    public String getColor() {
+        return color;
+    }
+    public void setColor(String color) {
+        this.color = color;
+    }
+    public LocalDate getReleaseDate() {
+        return releaseDate;
+    }
+    public void setReleaseDate(LocalDate releaseDate) {
+        this.releaseDate = releaseDate;
+    }
+
+    
+
+}
