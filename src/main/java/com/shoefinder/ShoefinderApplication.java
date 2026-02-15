4d5536a78d0281d0ef80184e0e0b8e23011668c4
Gui <guidaltrino@gmail.com>
2026-02-15 14:43:07 -0300

    Creating initial  Domains

--- src/main/java/com/shoefinder/ShoefinderApplication.java
+++ src/main/java/com/shoefinder/ShoefinderApplication.java
@@ -1,4 +1,4 @@
-package com.shoefinder.shoefinder;
+package com.shoefinder;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
