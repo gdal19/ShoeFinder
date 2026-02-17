015d42033e80875dee475375be677dd7b4639b0b
Gui <guidaltrino@gmail.com>
2026-02-17 18:01:03 -0300

    Creating initial methods for services

--- src/main/java/com/shoefinder/service/ShoeService.java
+++ src/main/java/com/shoefinder/service/ShoeService.java
@@ -1,5 +1,25 @@
 package com.shoefinder.service;
 
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+import com.shoefinder.repository.ShoeRepository;
+import com.shoefinder.domain.Shoe;
+
+@Service
 public class ShoeService {
 
+    private final ShoeRepository repository;
+
+    public ShoeService (ShoeRepository repository){
+        this.repository = repository;
+    }
+
+    public Shoe addShoe(Shoe shoe){
+        return repository.save(shoe); 
+    }
+
+    public List<Shoe> getShoes(){
+        return repository.findAll();
+    }
 }
--- src/main/java/com/shoefinder/service/UserService.java
+++ src/main/java/com/shoefinder/service/UserService.java
@@ -1,5 +1,25 @@
 package com.shoefinder.service;
 
-public class UserService {
+import java.util.List;
 
+import org.springframework.stereotype.Service;
+
+import com.shoefinder.domain.User;
+import com.shoefinder.repository.UserRepository;
+
+@Service
+public class UserService {
+    private final UserRepository repository;
+
+    public UserService (UserRepository repository){
+        this.repository = repository;
+    }
+
+    public User addUser(User user){
+        return repository.save(user);
+    }
+
+    public List<User> getUsers (){
+        return repository.findAll();
+    }
 }
