df8ac99287dc7dbfd5752dc061a0de596521aa65
Gui <guidaltrino@gmail.com>
2026-05-16 18:04:14 -0300

    Adding ShoeCard component

---
+++ frontend/shoefinder/src/components/ShoeCard.jsx
@@ -0,0 +1,12 @@
+
+function ShoeCard({name, price}) {
+
+    return (
+        <div> 
+            <h2>{name}</h2>
+            <p>R$ {price}</p>
+        </div>
+    )
+}
+
+export default ShoeCard
--- frontend/shoefinder/src/pages/Home.jsx
+++ frontend/shoefinder/src/pages/Home.jsx
@@ -1,14 +1,19 @@
 import { useState } from "react"
+import ShoeCard from "../components/ShoeCard"
 
 function Home(){
     const [message, setMessage] = useState("Welcome")
     let appName = "Shoe Finder"
     //let message = "Welcome"
 
+    const shoes = [
+        {id: 1, name: "Jordan 1", price: 1300},
+        {id: 2, name : "Curry 6", price: 1100}
+    ]
+
     function newMessage(){
         setMessage("botao clicado")
     }
-
 
     return(
         <div>
@@ -17,6 +22,13 @@ function Home(){
             <button onClick={newMessage}> 
                 CLICK HERE!!!
             </button>
+            {
+                shoes.map((shoe) => { 
+                    return (
+                        <ShoeCard key = {shoe.id} name = {shoe.name} price={shoe.price} />
+                    )
+                })
+            }
         </div>
     )
 }
