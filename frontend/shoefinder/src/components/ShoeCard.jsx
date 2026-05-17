19fd284f624d9e3bc7302e2efdc5eecf11e8f877
Gui <guidaltrino@gmail.com>
2026-05-17 14:28:29 -0300

    Adding search bar and images to cards

--- frontend/shoefinder/src/components/ShoeCard.jsx
+++ frontend/shoefinder/src/components/ShoeCard.jsx
@@ -1,9 +1,10 @@
 import "./ShoeCard.css"
 
-function ShoeCard({name, price}) {
+function ShoeCard({name, price, image}) {
 
     return (
         <div className="shoe-card"> 
+            <img className="shoe-image" src={image} alt={name}/>
             <h2>{name}</h2>
             <p>R$ {price}</p>
         </div>
--- frontend/shoefinder/src/pages/Home.jsx
+++ frontend/shoefinder/src/pages/Home.jsx
@@ -1,33 +1,43 @@
 import { useState } from "react"
 import ShoeCard from "../components/ShoeCard"
 import "./Home.css"
+import jordan1 from "../assets/images/jordan1.jpg.webp"
+import jordan3 from "../assets/images/jordan3.webp"
+import curry6 from "../assets/images/curry6.jpg.avif"
 
 function Home(){
-    const [message, setMessage] = useState("Welcome")
+    const [search, setSearch] = useState("")
     let appName = "Shoe Finder"
-    //let message = "Welcome"
+    let message = "Welcome"
 
     const shoes = [
-        {id: 1, name: "Jordan 1", price: 1300},
-        {id: 2, name : "Curry 6", price: 1100}
+        {id: 1, name: "Jordan 1", price: 1300, image: jordan1},
+        {id: 2, name : "Curry 6", price: 1100, image: curry6},
+        {id: 3, name : "Jordan 3", price: 1200, image: jordan3}
     ]
 
-    function newMessage(){
-        setMessage("botao clicado")
+    function newSearch(event){
+        // console.log(event)
+        // console.log(event.target)
+        // console.log(event.target.value)
+        setSearch(event.target.value)
     }
+
+    const filteredShoes = shoes.filter((shoe) => {
+        return shoe.name.includes(search)}
+    )
 
     return(
         <div className="home-container">
             <h1>{appName}</h1>
             <p>{message}</p>
-            <button onClick={newMessage}> 
-                CLICK HERE!!!
-            </button>
+            <input className="search-bar" type ="text" placeholder="Search..." value={search} onChange={newSearch}/>
+            <p>{search}</p>
             <div className="shoe-list">
                 {
-                    shoes.map((shoe) => { 
+                    filteredShoes.map((shoe) => { 
                         return (
-                            <ShoeCard key = {shoe.id} name = {shoe.name} price={shoe.price} />
+                            <ShoeCard key = {shoe.id} name = {shoe.name} price={shoe.price} image={shoe.image} />
                         )
                     })
                 }
